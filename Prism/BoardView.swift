40d094df83359ccea97282055e18d8a0b80bd226
Invader Zim <pwdelete@gmail.com>
2025-02-02 13:31:07 -0800

    Removed test code.

--- Prism/BoardView.swift
+++ Prism/BoardView.swift
@@ -10,13 +10,9 @@ import UIKit
 class BoardView: UIView {
 
     let prism = UIBezierPath()
-    var rectangle = UIBezierPath()
 
     override func draw(_ rect: CGRect) {
-        print("BoardView.draw")
         drawPrism()
-        drawRectangle()
-        drawPoint()
     }
     
     private func drawPrism() {
@@ -24,25 +20,7 @@ class BoardView: UIView {
         prism.addLine(to: CGPoint(x: 300, y: 300))
         prism.addLine(to: CGPoint(x: 100, y: 300))
         prism.close()
-//        prism.setLineDash([5, 5], count: 2, phase: 0)
-        
-        let newPath = prism.cgPath.copy(strokingWithWidth: 3, lineCap: CGLineCap.butt, lineJoin: CGLineJoin.round, miterLimit: 0)
-        let newBezier = UIBezierPath(cgPath: newPath)
-        UIColor.green.setStroke()
-        newBezier.stroke()
-
         UIColor.red.setStroke()
         prism.stroke()
-    }
-    
-    private func drawRectangle() {
-        UIColor.blue.setStroke()
-        rectangle.stroke()
-    }
-    
-    private func drawPoint() {
-        let point = UIBezierPath(arcCenter: Constant.insidePoint, radius: 3, startAngle: 0, endAngle: 2 * .pi, clockwise: true)
-        UIColor.black.setFill()
-        point.fill()
     }
 }
--- Prism/ViewController.swift
+++ Prism/ViewController.swift
@@ -7,76 +7,13 @@
 
 import UIKit
 
-struct Constant {
-    static let insidePoint = CGPoint(x: 100, y: 150)  // inside boardView.prism
-    static let outsidePoint = CGPoint(x: 100, y: 100)  // outside boardView.prism
-}
-
 class ViewController: UIViewController {
         
     @IBOutlet weak var boardView: BoardView!
     
     override func viewDidLoad() {
-        print("viewDidLoad")
         super.viewDidLoad()
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [unowned self] in
-            testIsInside()
-            testIsInside2()
-            testIsInside3()
-        }
     }
-    
-    private func testIsInside() {
-        let circle = UIBezierPath(arcCenter: Constant.insidePoint, radius: 10, startAngle: 0, endAngle: 2 * .pi, clockwise: true)
-        if circle.containsInside(Constant.insidePoint, toleranceWidth: 3.0) {
-            print("point is inside circle (not shown)")
-        } else {
-            print("point is outside circle")
-        }
-    }
-    
-    private func testIsInside2() {
-        if boardView.prism.containsInsideBorder(Constant.insidePoint, toleranceWidth: 3.0) {
-            print("point touches prism")
-        } else {
-            print("point doesn't touch prism")
-        }
-    }
-    
-//    private func testIsInside3() {
-//        boardView.rectangle = UIBezierPath(roundedRect: CGRect(x: 50, y: 150, width: 300, height: 200), cornerRadius: 0)
-//        boardView.setNeedsDisplay()
-//        if boardView.rectangle.containsInsideBorder(Constant.insidePoint, toleranceWidth: 3.0) {
-//            print("point is inside rectangle")
-//        } else {
-//            print("point is outside rectangle")
-//        }
-//    }
-    
-    private func testIsInside3() {
-        let rect = UIBezierPath(roundedRect: CGRect(x: 50, y: 150, width: 300, height: 200), cornerRadius: 0)
-        let rectPath = rect.cgPath.copy(strokingWithWidth: 10, lineCap: CGLineCap.butt, lineJoin: CGLineJoin.round, miterLimit: 0)
-        boardView.rectangle = UIBezierPath(cgPath: rectPath)
-        boardView.setNeedsDisplay()
-        if rect.containsInsideBorder(Constant.insidePoint, toleranceWidth: 3.0) {
-            print("point touches rectangle")
-        } else {
-            print("point doesn't touch rectangle")
-        }
-    }
-    
-//    private func testIsInside3() {
-//        // actually a circle, but wanted to piggy-back on boardView.rectangle
-//        let rect = UIBezierPath(arcCenter: Constant.insidePoint, radius: 100, startAngle: 0, endAngle: 2 * .pi, clockwise: true)
-////        let rectPath = rect.cgPath.copy(strokingWithWidth: 10, lineCap: CGLineCap.butt, lineJoin: CGLineJoin.round, miterLimit: 0)
-//        boardView.rectangle = UIBezierPath(cgPath: rect.cgPath)
-//        boardView.setNeedsDisplay()
-//        if boardView.rectangle.containsInsideBorder(Constant.insidePoint, toleranceWidth: 3.0) {  // should always be true (is false!!!)
-//            print("point is inside circle")
-//        } else {
-//            print("point is outside circle")
-//        }
-//    }
 }
 
 // from: https://stackoverflow.com/a/71279467/2526464
