61b06009c6bac778dfb327280eeea1149239365b
Invader Zim <pwdelete@gmail.com>
2025-02-04 17:38:18 -0800

    Model a range of light wavelengths passing through the prism (defraction rainbow).

--- Prism/BoardView.swift
+++ Prism/BoardView.swift
@@ -8,11 +8,13 @@
 import UIKit
 
 struct Constant {
+    static let lightWaveLengths = stride(from: 400.0, through: 680.0, by: 40.0)
     static let prismSideLength = 180.0
     static let lightSourceSideLength = 140.0  // view size (bigger than drawing)
     static let refractiveIndexOfAir = 1.0
     static let refractiveIndexOfGlass = 1.53  // (eventually make it a function of light wavelength)
-    static let lightSourceStartingCenter = CGPoint(x: 50, y: 320)
+    static let prismStartingCenter = CGPoint(x: 200, y: 150)
+    static let lightSourceStartingCenter = CGPoint(x: 50, y: 180)
     static let lightSourceStartingDirection = -20.rads  // +/-.pi radians (0 right, positive clockwise)
     static let lightPropagationStepSize = 2.0
 }
@@ -43,7 +45,8 @@ class BoardView: UIView {
     }
     
     private func setupPrismView() {
-        prismView.frame = CGRect(x: 100, y: 200, width: Constant.prismSideLength, height: Constant.prismSideLength * sin(60.rads))
+        prismView.center = Constant.prismStartingCenter
+        prismView.bounds.size = CGSize(width: Constant.prismSideLength, height: Constant.prismSideLength * sin(60.rads))
         prismView.backgroundColor = .clear
         addSubview(prismView)
         
@@ -77,10 +80,20 @@ class BoardView: UIView {
         guard isOnScreen(point) else { return }
         
         // continue propagating each individual color through prism and beyond
-        continuePropagatingIndividualColor(.green, startingLightDirection: lightDirection, startingPoint: point)
+        for wavelength in Constant.lightWaveLengths {
+            continuePropagatingLightWith(wavelength: wavelength, startingLightDirection: lightDirection, startingPoint: point)
+        }
+//        for (index, wavelength) in Constant.lightWaveLengths.enumerated() {
+//            // offset slightly, so last color (red) doesn't start out covering everything
+//            let offset = CGPoint(x: 0.2 * Double(index), y: 0.2 * Double(index))
+//            continuePropagatingLightWith(wavelength: wavelength, startingLightDirection: lightDirection, startingPoint: point + offset)
+//        }
     }
     
-    private func continuePropagatingIndividualColor(_ color: UIColor, startingLightDirection: Double, startingPoint: CGPoint) {
+    private func continuePropagatingLightWith(wavelength: Double, startingLightDirection: Double, startingPoint: CGPoint) {
+        let color = colorForWavelength(wavelength)
+        let refractiveIndexOfGlass = refractiveIndexOfGlassWithWavelength(wavelength)
+        
         var point = startingPoint
         var mediumsTraversed = 0
         var lightDirections = [startingLightDirection]
@@ -88,7 +101,10 @@ class BoardView: UIView {
         light.move(to: point)
 
         // bend light at prism interface
-        let lightDirectionPrism = lightDirectionOut(lightDirectionIn: lightDirections[mediumsTraversed], point: point, isEnteringPrism: true)
+        let lightDirectionPrism = lightDirectionOut(lightDirectionIn: lightDirections[mediumsTraversed],
+                                                    point: point,
+                                                    refractiveIndexOfGlass: refractiveIndexOfGlass,
+                                                    isEnteringPrism: true)
         lightDirections.append(lightDirectionPrism)
         mediumsTraversed += 1
         
@@ -102,7 +118,10 @@ class BoardView: UIView {
         guard isOnScreen(point) else { finishDrawingLight(light, color: color); return }
         
         // bend light at air interface
-        let lightDirectionAir = lightDirectionOut(lightDirectionIn: lightDirections[mediumsTraversed], point: point, isEnteringPrism: false)
+        let lightDirectionAir = lightDirectionOut(lightDirectionIn: lightDirections[mediumsTraversed],
+                                                  point: point,
+                                                  refractiveIndexOfGlass: refractiveIndexOfGlass,
+                                                  isEnteringPrism: false)
         lightDirections.append(lightDirectionAir)
         mediumsTraversed += 1
         
@@ -118,7 +137,7 @@ class BoardView: UIView {
     
     private func finishDrawingLight(_ light: UIBezierPath, color: UIColor) {
         color.setStroke()
-        light.lineWidth = 2
+        light.lineWidth = 1
         light.stroke()
     }
     
@@ -133,11 +152,11 @@ class BoardView: UIView {
     }
     
     // light direction after crossing surface boundary
-    private func lightDirectionOut(lightDirectionIn: Double, point: CGPoint, isEnteringPrism: Bool) -> Double {
+    private func lightDirectionOut(lightDirectionIn: Double, point: CGPoint, refractiveIndexOfGlass: Double, isEnteringPrism: Bool) -> Double {
         let surfaceNormalAngle = surfaceNormalAngleAtPoint(point)! + (isEnteringPrism ? 0 : .pi)
 //        drawVectorAt(point, inDirection: surfaceNormalAngle, color: .cyan)
         let angleOfIncidence = surfaceNormalAngle - lightDirectionIn
-        var refractionRatio = Constant.refractiveIndexOfAir / Constant.refractiveIndexOfGlass
+        var refractionRatio = Constant.refractiveIndexOfAir / refractiveIndexOfGlass
         if !isEnteringPrism { refractionRatio = 1 / refractionRatio }
         var sinTheta2 = (refractionRatio * sin(angleOfIncidence))
         let angleOfRefraction = asin(sinTheta2.limitedBetween(-1, and: 1))
@@ -148,7 +167,7 @@ class BoardView: UIView {
     // direction of surface normal pointing toward inside of prismView
     // +/-.pi radians (0 right, positive clockwise)
     private func surfaceNormalAngleAtPoint(_ point: CGPoint) -> Double? {
-        let radius = 3.0
+        let radius = 4.0
         let numPoints = 180  // every 2 degrees
         let deltaAngle = 2 * .pi / Double(numPoints)
         var isContainedArray = [Bool]()
@@ -173,6 +192,68 @@ class BoardView: UIView {
         color.setStroke()
         normal.setLineDash([5, 5], count: 2, phase: 0)
         normal.stroke()
+    }
+    
+    // from: https://www.koppglass.com/blog/optical-properties-glass-how-light-and-glass-interact
+    private func refractiveIndexOfGlassWithWavelength(_ wavelength: Double) -> Double {
+        1.61 - 0.00024121 * wavelength + 0.00000016 * wavelength * wavelength
+    }
+    
+    // from: https://stackoverflow.com/a/14917481/2526464
+    private func colorForWavelength(_ wavelength: Double) -> UIColor {
+        var factor = 0.0
+        var red = 0.0
+        var green = 0.0
+        var blue = 0.0
+        
+        if((wavelength >= 380) && (wavelength < 440)) {
+            red = -(wavelength - 440) / (440 - 380)
+            green = 0.0
+            blue = 1.0
+        } else if((wavelength >= 440) && (wavelength < 490)) {
+            red = 0.0
+            green = (wavelength - 440) / (490 - 440)
+            blue = 1.0
+        } else if((wavelength >= 490) && (wavelength < 510)) {
+            red = 0.0
+            green = 1.0
+            blue = -(wavelength - 510) / (510 - 490)
+        } else if((wavelength >= 510) && (wavelength < 580)) {
+            red = (wavelength - 510) / (580 - 510)
+            green = 1.0
+            blue = 0.0
+        } else if((wavelength >= 580) && (wavelength < 645)) {
+            red = 1.0
+            green = -(wavelength - 645) / (645 - 580)
+            blue = 0.0
+        } else if((wavelength >= 645) && (wavelength < 781)) {
+            red = 1.0
+            green = 0.0
+            blue = 0.0
+        } else {
+            red = 0.0
+            green = 0.0
+            blue = 0.0
+        }
+        
+        // Let the intensity fall off near the vision limits
+        if((wavelength >= 380) && (wavelength < 420)) {
+            factor = 0.3 + 0.7 * (wavelength - 380) / (420 - 380)
+        } else if((wavelength >= 420) && (wavelength < 701)) {
+            factor = 1.0
+        } else if((wavelength >= 701) && (wavelength < 781)) {
+            factor = 0.3 + 0.7 * (780 - wavelength) / (780 - 700)
+        } else {
+            factor = 0.0
+        }
+        
+        let gamma = 0.8
+        
+        red = pow(red * factor, gamma)
+        green = pow(green * factor, gamma)
+        blue = pow(blue * factor, gamma)
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
     }
 
     // MARK: - Gestures handlers
